850fadbddd9b03dee9bfddd758f173b81ef198b7
Kittipot Buaket <kittipot1999@hotmail.com>
2021-08-09 22:14:57 +0700

    sad

---
+++ src/Component/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <p className='text-center text-gray-400'>Copyright © 2021 By Kittipot Buaket</p>
+    )
+}
+
+export default Footer
